010235e7a4b7a2a06af835a9af63c4a15d0ddfc6
samuelmayna <samuelmayna@gmail.com>
2022-02-07 18:32:23 +0300

    Added the ray class. It will be responsible for creating ray from one source to another.

---
+++ qbRayTrace/ray.hpp
@@ -0,0 +1,22 @@
+#ifndef RAY_H
+#define RAY_H
+#include "./qbLinAlg/qbVector.h"
+
+namespace qbRT
+{
+    class Ray
+    {
+    public:
+        Ray(const qbVector<double> &point1, const qbVector<double> &point2);
+
+        qbVector<double> GetPoint1() const;
+        qbVector<double> GetPoint2() const;
+
+    private:
+        qbVector<double> m_point1(3);
+        qbVector<double> m_point2(3);
+        qbVector<double> m_lab(3);
+    }
+}
+
+#endif
