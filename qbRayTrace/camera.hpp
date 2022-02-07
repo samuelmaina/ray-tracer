4e94388fef9543053ebe4816539b029b5b7967de
samuelmayna <samuelmayna@gmail.com>
2022-02-07 19:42:45 +0300

    added the function for the camera

---
+++ qbRayTrace/camera.hpp
@@ -0,0 +1,57 @@
+#ifndef CAMERA_H
+#define CAMERA_H
+#include "./qbLinAlg/qbVector.h"
+#include "ray.hpp"
+namespace qbRT
+{
+    class Camera
+    {
+    private:
+    public:
+        Camera();
+        //function to set the camera parameter
+        void SetPosition(const qbVector<double> &newPosition);
+        void SetLookAt(const qbVector<double> &newLookAt);
+        void SetUp(const qbVector<double> &upVector);
+        void SetLength(double newLength);
+        void SetAspect(double newAspect);
+        qbVector<double> GetPosition();
+        qbVector<double> GetLookAt();
+        qbVector<double> GetUp();
+        qbVector<double> GetU();
+        qbVector<double> GetW();
+        qbVector<double> GetScreenCentre();
+
+        double GetLength();
+        double GetHorizontalSize();
+
+        Ray GenerateRay(float proScreenX, float proScreenY);
+        void UpdateCameraGeometry();
+
+    private:
+        qbVector<double> m_position(3);
+        qbVector<double> m_lookAt(3);
+        qbVector<double> m_cameraUp(3);
+        double m_length;
+        double m_horiLength;
+        double m_aspectRatio;
+
+        qbVector<double> m_alignVector(3);
+        qbVector<double> m_projectionScreenU(3);
+        qbVector<double> m_projectionScreenV(3);
+        qbVector<double> m_projectionScreenCentre(3);
+
+        ~Camera();
+    };
+
+    Camera::Camera(/* args */)
+    {
+    }
+
+    Camera::~Camera()
+    {
+    }
+
+}
+
+#endif
