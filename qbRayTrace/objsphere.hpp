849b24704ee835b20626fb9ed83e998743096c1c
samuelmayna <samuelmayna@gmail.com>
2022-02-08 12:18:52 +0300

    Add  functions that will be used to create spheres

---
+++ qbRayTrace/objsphere.hpp
@@ -0,0 +1,20 @@
+#ifndef OBJSPHERE_H
+#define OBJSPHERE_H
+
+#include "objectbase.hpp"
+
+namespace qbRT
+{
+    class ObjSphere : public qbRT::ObjectBase
+    {
+    public:
+        //used to create a unit(radius 1) from the origin.
+        ObjSphere();
+        virtual ~ObjSphere() override;
+        virtual bool TestIntersection(const qbRT::Ray &castRay, qbVector<double> &intPoint, qbVector<double> &localNormal, qbVector<double> &localColor);
+
+    private:
+    };
+};
+
+#endif
