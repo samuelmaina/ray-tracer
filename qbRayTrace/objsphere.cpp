dd08bf365f18ca2e3754fcb55dbed0e819a6b735
samuelmayna <samuelmayna@gmail.com>
2022-02-08 16:53:54 +0300

    modified the code to calculate the  closest point of intersection and return it

--- qbRayTrace/objsphere.cpp
+++ qbRayTrace/objsphere.cpp
@@ -23,5 +23,28 @@ bool qbRT::ObjSphere::TestIntersection(const qbRT::Ray &castRay, qbVector<double
     double c = qbVector<double>::dot(point1, point1) - 1.0;
 
     double test = (b * b) - 4.0 * c;
-    return test > 0.0;
+    if (test > 0.0)
+    {
+        double numSQRT = sqrtf(test);
+        double t1 = (-b + numSQRT) / 2.0;
+        double t2 = (-b - numSQRT) / 2.0;
+        //if either t1 or t2 is less that 0, then there is some part
+        //of the object that is behind the camera, hence ignore the point.
+
+        if ((t1 < 0.0) || (t2 < 0.0))
+            return false;
+        //t1, and t2, will give the forward and the backward point under which the ray cuts the sphere.
+        //The point that is closest to the camera is given by the less of t1 or t2
+        if (t1 < t2)
+        {
+            intPoint = point1 + (vhat * t1);
+        }
+        else
+        {
+            intPoint = point1 + (vhat * t2);
+        }
+
+        return true;
+    }
+    return false;
 }
