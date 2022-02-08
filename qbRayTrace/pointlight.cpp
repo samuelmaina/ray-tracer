6e0d3e4b7ec4263e63e104ff1d26c607f73bf05c
samuelmayna <samuelmayna@gmail.com>
2022-02-08 18:39:22 +0300

    Added a method that will calculate the intensity of the light

---
+++ qbRayTrace/pointlight.cpp
@@ -0,0 +1,41 @@
+#include "pointlight.hpp"
+#include "math.h"
+
+#define _USE_MATH_DEFINES
+qbRT::PointLight::PointLight()
+{
+    color = qbVector<double>{
+        std::vector<double>{1.0, 1.0, 1.0}};
+}
+
+qbRT::LightBase::~LightBase()
+{
+}
+bool qbRT::PointLight::ComputeIllumination(const qbVector<double> &intPoint, const qbVector<double> &localNormal, const std::vector<std::shared_ptr<qbRT::ObjectBase>> &objectList, const std::shared_ptr<qbRT::ObjectBase> &currentObject, qbVector<double> &color, double &intensity)
+{
+
+    qbVector<double> lightDir = (location - intPoint).Normalized();
+    qbVector<double> startPoint = intPoint;
+
+    //compute the the angle between the light ray and the local normal.
+    // The normal vector is  assumed to be assumed to normalized.
+    //Since the normal and the lightVector are assumed to be unit vector their
+    //produce will be just be 1, which will make it easy for us to compute the
+    //angle between ray and the normal.
+    double angle = acos(qbVector<double>::dot(localNormal, lightDir));
+
+    //if the normal is point away from the  light source, then there is no illumination.
+    double angle_ninety = acos(0.0);
+    if (angle > angle_ninety)
+    {
+        color = this->color;
+        intensity = 0.0;
+        return false;
+    }
+    else
+    {
+        color = this->color;
+        intensity = this->intesity * (1.0 - (angle / angle_ninety));
+        return true;
+    }
+}
