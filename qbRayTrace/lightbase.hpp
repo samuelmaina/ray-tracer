50f2c9a900ed02f227d75581ae6b48e6346b0dc5
samuelmayna <samuelmayna@gmail.com>
2022-02-08 17:56:47 +0300

    add file that will determine the light intensity of the objects

---
+++ qbRayTrace/lightbase.hpp
@@ -0,0 +1,25 @@
+#ifndef LIGHTBASE_H
+#define LIGHTBASE_H
+#include <memory>
+#include "./qbLinAlg/qbVector.h"
+#include "ray.hpp"
+#include "objectbase.hpp"
+
+namespace qbRT
+{
+    class LightBase
+    {
+    private:
+    public:
+        LightBase();
+        virtual ~LightBase();
+        virtual bool ComputerIllumination(const qbVector<double> &intPoint, const qbVector<double> &localNormal, const std::vector<std::shared_ptr<qbRT::ObjectBase>> &objectList, const std::shared_ptr<qbRT::ObjectBase> &currentObject, qbVector<double> &color, double &intensity);
+
+    public:
+        qbVector<double> color{3};
+        qbVector<double> location{3};
+        double intesity;
+    };
+}
+
+#endif
