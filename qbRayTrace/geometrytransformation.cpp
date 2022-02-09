2cc68fee557a141198e6812a0357437233a3c71d
samuelmayna <samuelmayna@gmail.com>
2022-02-09 13:58:56 +0300

    Added the constructor and destructor for the rotation class. Implemented main function that is used to give a single forward adn backward transformation

---
+++ qbRayTrace/geometrytransformation.cpp
@@ -0,0 +1,79 @@
+#include "geometrytransformation.hpp"
+#include "math.h"
+
+qbRT::GTForm::GTForm()
+{
+    //set the forward and backward transforms to identity
+    //matrices to avoid erroneous behaviour.
+    forwardTransform.SetToIdentity();
+    backwardTransform.SetToIdentity();
+};
+qbRT::GTForm::~GTForm()
+{
+}
+
+qbRT::GTForm::GTForm(const qbMatrix2<double> &fwd, const qbMatrix2<double> &bck)
+{
+    //reject if any inputs are  not 4x4
+    if (!(fwd.GetNumRows() == 4 && fwd.GetNumCols() == 4 && bck.GetNumRows() == 4 && bck.GetNumCols() == 4))
+    {
+        throw std::invalid_argument("either the forward or the backward matrices are wrong.");
+    }
+    forwardTransform = fwd;
+    backwardTransform = bck;
+}
+
+//function to set the transformation.
+void qbRT::GTForm::SetTransform(const qbVector<double> &translation, const qbVector<double> &rotation, const qbVector<double> &scale)
+{
+    qbMatrix2<double> translationMatrix{4, 4};
+    qbMatrix2<double> rotationMatrixX{4, 4};
+    qbMatrix2<double> rotationMatrixY{4, 4};
+    qbMatrix2<double> rotationMatrixZ{4, 4};
+    qbMatrix2<double> scaleMatrix{4, 4};
+
+    //First set all to identity matrices.
+    translationMatrix.SetToIdentity();
+    rotationMatrixX.SetToIdentity();
+    rotationMatrixY.SetToIdentity();
+    rotationMatrixZ.SetToIdentity();
+    scaleMatrix.SetToIdentity();
+
+    //popluate the matrices with the appropriate values.
+    //first start with the translation matrix.
+    translationMatrix.SetElement(0, 3, translation.GetElement(0));
+    translationMatrix.SetELement(1, 3, translation.GetElement(1));
+    translationMatrix.SetElement(2, 3, translation.GetELement(2));
+
+    //Rotation Matrices.
+    double zAngle = rotation.GetElement(2);
+    rotationMatrixZ.SetlElement(0, 0, cos(zAngle));
+    rotationMatrixZ.SetElement(0, 1, -sin(zAngle));
+    rotationMatrixZ.SetElement(1, 0, sin(zAngle));
+    rotationMatrixZ.SetElement(1, 1, cos(zAngle));
+
+    double yAngle = rotation.GetElement(1);
+    rotationMatrixY.SetlElement(0, 0, cos(yAngle));
+    rotationMatrixY.SetElement(0, 2, sin(yAngle));
+    rotationMatrixY.SetElement(2, 0, -sin(yAngle));
+    rotationMatrixY.SetElement(2, 2, cos(yAngle));
+
+    double xAngle = rotation.GetElement(0);
+    rotationMatrixZ.SetlElement(1, 1, cos(xAngle));
+    rotationMatrixZ.SetElement(1, 2, -sin(xAngle));
+    rotationMatrixZ.SetElement(2, 1, sin(xAngle));
+    rotationMatrixZ.SetElement(2, 2, cos(xAngle));
+
+    //And the scale Matrix.
+    scaleMatrix.SetElement(0, 0, scale.GetElement(0));
+    scaleMatrix.SetElement(1, 1, scale.GetElement(1));
+    scaleMatrix.SetElement(2, 2, scale.GetElement(2));
+
+    //do the final transform Matrix.
+    //asumption that by default the use is making a forward transformation.
+    forwardTransform = translationMatrix * scaleMatrix * rotationMatrixX * rotationMatrixY * rotationMatrixZ;
+
+    backwardTransform = forwarTransform;
+    //it is now the inverse of the forwardTransformation.
+    backwardTransform.Inverse();
+}
