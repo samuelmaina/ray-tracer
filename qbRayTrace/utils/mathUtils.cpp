ff1b15a329ed77f10798c35998feea58d6b94155
samuelmayna <samuelmayna@gmail.com>
2022-03-25 09:46:03 +0300

    Add a function that will be used to calculate the roots of quadratic equations in the program

--- qbRayTrace/utils/mathUtils.cpp
+++ qbRayTrace/utils/mathUtils.cpp
@@ -11,3 +11,18 @@ qbVector<double> ConstructFinalColor(const double red, const double green, const
 {
     return ConstructVector(red * baseColor.GetElement(0), green * baseColor.GetElement(1), blue * baseColor.GetElement(2));
 }
+
+std::vector<double> GetRoots(double a, double b, double c)
+{
+    std::vector<double> ans;
+    double twoA = 2.0 * a;
+    double det = pow(b, 2.0) - 4 * a * c;
+    // we are working in plane and we don't expect any complex numbers
+    // hence throw error  when the roots are complex
+    if (det < 0)
+        throw "The quadratic equation can not have real roots";
+    double d = sqrt(det);
+    ans.push_back((-b + d) / twoA);
+    ans.push_back((-b - d) / twoA);
+    return ans;
+}
--- qbRayTrace/utils/mathUtils.hpp
+++ qbRayTrace/utils/mathUtils.hpp
@@ -1,9 +1,13 @@
 #ifndef MATHUTILS_H
 #define MATHUTILS_H
 
+#include <vector>
+#include <cmath>
 #include "../qbLinAlg/qbVector.h"
 #include "../utils/vectormanipulators.hpp"
 void ComputeColorIntensity(const qbVector<double> color, double &red, double &green, double &blue, const double intensity);
 qbVector<double> ConstructFinalColor(const double red, const double green, const double blue, const qbVector<double> baseColor);
+
+std::vector<double> GetRoots(double a, double b, double c);
 
 #endif
