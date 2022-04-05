0469e731b2e06eab4a9129bab70487101856d698
samuelmayna <samuelmayna@gmail.com>
2022-04-05 23:14:49 +0300

    Refactored the Test Intesection to have a function that will be getting t values and calculating the different vector at the point of intersection

--- qbRayTrace/primitives/objplane.cpp
+++ qbRayTrace/primitives/objplane.cpp
@@ -18,42 +18,48 @@ bool qbRT::ObjPlane::TestIntersection(const qbRT::Ray &castRay, qbVector<double>
     // get the ray vector and normalize it.
     qbVector<double> k = bckRay.GetRayVector(), point1 = bckRay.GetPoint1();
     k.Normalize();
-
     // check if there is an intersection , i.e. if the castRay is not parallel to
     // to the plane
     if (!CloseEnough(k.GetElement(2), 0.0))
     {
-        double t = point1.GetElement(2) / (-k.GetElement(2));
+        return CalculateIntersection(point1, k, intPoint, localNormal, localColor);
+    }
+    return false;
+}
 
-        /*If t is negative, then the intersection point must be behind
-         the camera and we ignore it.*/
-        if (t > 0.0)
+bool qbRT::ObjPlane::CalculateIntersection(qbVector<double> &origin, qbVector<double> &rayVector, qbVector<double> &intPoint,
+                                           qbVector<double> &localNormal, qbVector<double> &localColor)
+{
+    double originZElem = origin.GetElement(2), rayVectorZDir = rayVector.GetElement(2);
+    double t = originZElem / (-rayVectorZDir);
+
+    /*If t is negative, then the intersection point must be behind
+     the camera and we ignore it.*/
+    if (t > 0.0)
+    {
+        // compute the values of u and v.
+        double u = origin.GetElement(0) + t * rayVector.GetElement(0);
+        double v = origin.GetElement(1) + rayVector.GetElement(1) * t;
+        // if both magnitudes of u and v are less than equlalto 1
+        //  then they must be on the plane.
+
+        if (abs(u) <= 1.0 && abs(v) <= 1.0)
         {
-            // compute the values of u and v.
-            double u = point1.GetElement(0) + t * k.GetElement(0);
-            double v = point1.GetElement(1) + k.GetElement(1) * t;
-            // if both magnitudes of u and v are less than equlalto 1
-            //  then they must be on the plane.
 
-            if (abs(u) <= 1.0 && abs(v) <= 1.0)
-            {
+            // compute the point of intersection.
+            qbVector<double> poi = origin + t * rayVector;
 
-                // compute the point of intersection.
-                qbVector<double> poi = point1 + t * k;
+            // transform the intersection point back into world coordinates.
+            intPoint = trnfrmMatrix.Apply(poi, qbRT::FWDTFORM);
 
-                // transform the intersection point back into world coordinates.
-                intPoint = trnfrmMatrix.Apply(poi, qbRT::FWDTFORM);
-
-                // compute the local normal.
-                qbVector<double> localOrigin = ConstructVector(0.0, 0.0, 0.0),
-                                 normalVector = ConstructVector(0.0, 0.0, -1.0),
-                                 globalOrigin = trnfrmMatrix.Apply(localOrigin, qbRT::FWDTFORM);
-                localNormal = trnfrmMatrix.Apply(normalVector, qbRT::FWDTFORM) - globalOrigin;
-                localNormal.Normalize();
-                localColor = baseColor;
-
-                return true;
-            }
+            // compute the local normal.
+            qbVector<double> localOrigin = ConstructVector(0.0, 0.0, 0.0),
+                             normalVector = ConstructVector(0.0, 0.0, -1.0),
+                             globalOrigin = trnfrmMatrix.Apply(localOrigin, qbRT::FWDTFORM);
+            localNormal = trnfrmMatrix.Apply(normalVector, qbRT::FWDTFORM) - globalOrigin;
+            localNormal.Normalize();
+            localColor = baseColor;
+            return true;
         }
         return false;
     }
--- qbRayTrace/primitives/objplane.hpp
+++ qbRayTrace/primitives/objplane.hpp
@@ -16,6 +16,9 @@ namespace qbRT
         virtual bool TestIntersection(const qbRT::Ray &castRay, qbVector<double> &intPoint,
                                       qbVector<double> &localNormal, qbVector<double> &localColor) override;
 
+        bool CalculateIntersection(qbVector<double> &origin, qbVector<double> &rayVector, qbVector<double> &intPoint,
+                                   qbVector<double> &localNormal, qbVector<double> &localColo);
+
     private:
     };
 };
