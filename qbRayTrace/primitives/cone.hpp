1fd616fb2d7983c37e64f492b88ddd1529bdee0f
samuelmayna <samuelmayna@gmail.com>
2022-08-17 07:52:31 +0300

    Had not added code of the cone.hpp and the implementation hpp file.

---
+++ qbRayTrace/primitives/cone.hpp
@@ -0,0 +1,26 @@
+
+#ifndef CONE_H
+#define CONE_H
+
+#include <cmath>
+#include "objectbase.hpp"
+#include "../geometrytransformation.hpp"
+
+namespace qbRT
+{
+    class Cone : public ObjectBase
+    {
+    public:
+        // Default constructor.
+        Cone();
+
+        // Override the destructor.
+        virtual ~Cone() override;
+
+        // Override the function to test for intersections.
+        virtual bool TestIntersection(const qbRT::Ray &castRay, qbVector<double> &intPoint,
+                                      qbVector<double> &localNormal, qbVector<double> &localColor) override;
+    };
+}
+
+#endif
